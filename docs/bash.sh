30844a506332af15a1bc23c3a572ae061b26ad0d
NightRaven <53287573+Xynnix@users.noreply.github.com>
2023-08-29 04:45:09 -0400

    Update bash.sh

--- docs/bash.sh
+++ docs/bash.sh
@@ -1,12 +1,14 @@
-sudo apt install lsb-release gnupg2 apt-transport-https ca-certificates curl software-properties-common -y
+#sudo apt install lsb-release gnupg2 apt-transport-https ca-certificates curl software-properties-common -y
+#curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/debian.gpg
+#sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+#sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+#sudo usermod -aG docker $USER
+#newgrp docker
+#sudo systemctl start docker && sudo systemctl enable docker
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/debian.gpg
-
-sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-
-sudo usermod -aG docker $USER
-newgrp docker
-
-sudo systemctl start docker && sudo systemctl enable docker
+mkdir -p /var/azuracast
+cd /var/azuracast
+curl -fsSL https://raw.githubusercontent.com/AzuraCast/AzuraCast/main/docker.sh > docker.sh
+chmod a+x docker.sh
+yes 'Y' | ./docker.sh setup-release
+yes '' | ./docker.sh install
